2b55184b16f00c016f8d74d4b5e397ca35497f99
SimoPed <103127889+SimoPed@users.noreply.github.com>
2022-11-15 11:40:28 +0100

    modify Film Card

--- lib/widgets/film_card.dart
+++ lib/widgets/film_card.dart
@@ -8,7 +8,8 @@ class FilmCard extends StatelessWidget {
   final String summary;
 
   const FilmCard(
-      {super.key, required this.name,
+      {super.key,
+      required this.name,
       required this.genres,
       required this.rating,
       required this.image,
@@ -16,15 +17,39 @@ class FilmCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Card(
-      color: Colors.red,
-      elevation: 20,
-      shadowColor: Colors.black,
+    return Container(
+      margin: const EdgeInsets.all(15),
+      decoration: BoxDecoration(
+          color: Colors.red,
+          borderRadius: BorderRadius.circular(10)
+      ),
       child: SizedBox(
-        width: 350,
+        width: 400,
         height: 150,
         child: Padding(
-          padding: EdgeInsets.all(10),
+          padding: const EdgeInsets.all(10),
+          child: Row(
+            children: [
+              Text(image, style: const TextStyle(
+              fontSize: 20,
+            ), textAlign: TextAlign.start,
+              ),
+              Column(
+                mainAxisSize: MainAxisSize.max,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Text(name, style: const TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  ), textAlign: TextAlign.center,
+                  ),
+                  Text(genres[0]),
+                  Text(summary),
+                  Text(rating.toString())
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
